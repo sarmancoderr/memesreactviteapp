7bff5d93082438f816663f8a6a162a06d62a67ac
Raúl Contreras <sarmancoder@gmail.com>
2023-05-28 11:18:20 +0200

    Mostrar broma

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,36 @@
+import { useEffect, useState } from "react"
+import { JOKES_HEADERS, URL_JOKES } from "./config"
+
 function App() {
+  const [punchline, setPunchline] = useState('')
+  const [setup, setSetup] = useState('')
+
+  const retrieveJoke = async () => {
+    const result = await fetch(URL_JOKES + '/random/joke', {
+      method: 'GET',
+      headers: JOKES_HEADERS
+    })
+
+    const data = await result.json()
+
+    const {punchline, setup} = data.body[0]
+
+    setPunchline(punchline)
+    setSetup(setup)
+  }
+
+  useEffect(() => {retrieveJoke()}, [])
+
   return (
-    <p>Aquí empieza la aplicaaación</p>
+    <>
+      <header>
+        <button onClick={() => {retrieveJoke()}}>Regenerate joke</button>
+      </header>
+      <section>
+        <h2>{punchline}</h2>
+        <p>{setup}</p>
+      </section>
+    </>
   )
 }
 
---
+++ src/config.ts
@@ -0,0 +1,6 @@
+export const URL_JOKES = 'https://dad-jokes.p.rapidapi.com'
+
+export const JOKES_HEADERS = {
+  'X-RapidAPI-Key': '56c60d8caemsh89a08a76935a033p1f0117jsnbdc7abc5d95b',
+  'X-RapidAPI-Host': 'dad-jokes.p.rapidapi.com'
+}
